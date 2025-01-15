7ef1dbb2020cd2f729fa301882c8bd07d433de02
danielrehn <daniel.rehn@consid.se>
2025-01-15 15:39:03 +0100

    Added ability to target the class attribute of prefab sub-components

--- prefabs/VgrSidebarButton.vue
+++ prefabs/VgrSidebarButton.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import {chevronRight} from "../icons";
-import {VgrButton, VgrIcon} from "../core";
+import {VgrAvatar, VgrButton, VgrIcon} from "../core";
 
-defineProps<{
+const props = defineProps<{
   is: string | object;
   to: string;
   active?: boolean;
   icon: string | object;
+  'class:icon'?: string;
+  'class:arrow'?: string;
 }>();
 </script>
 
@@ -15,10 +17,10 @@ defineProps<{
               :variant="active ? 'filled' : 'ghost'"
               expand="full" size="large" align="start" class="group"
   >
-    <vgr-icon :icon="icon" class="size-5 mr-1 -ml-1.5"/>
+    <vgr-icon :icon="icon" :class="[props['class:icon']]" class="size-5 mr-1 -ml-1.5"/>
     <span class="line-clamp-1">
       <slot/>
     </span>
-    <vgr-icon :icon="chevronRight" class="size-5 ml-auto transition-all opacity-0 group-hover:opacity-100 group-hover:translate-x-2 [.bg-healthcare>&]:opacity-100 [.bg-healthcare>&]:translate-x-2"/>
+    <vgr-icon :icon="chevronRight" :class="[props['class:arrow']]" class="size-5 ml-auto transition-all opacity-0 group-hover:opacity-100 group-hover:translate-x-2 [.bg-healthcare>&]:opacity-100 [.bg-healthcare>&]:translate-x-2"/>
   </vgr-button>
 </template>
