8eeb73871f770b89b1f7f135242d52a5f556d78f
danielrehn <daniel.rehn@consid.se>
2025-01-23 10:49:23 +0100

    Added some more icons

--- icons/index.ts
+++ icons/index.ts
@@ -8,14 +8,21 @@ export {default as plus} from './plus.svg?inline';
 
 // Outline
 export {default as bellOutline} from './bell-outline.svg?inline';
+export {default as calendarOutline} from './calendar-outline.svg?inline';
+export {default as chatBubbleOutline} from './chat-bubble-outline.svg?inline';
 export {default as circleOutline} from './circle-outline.svg?inline';
 export {default as clipboardOutline} from './clipboard-outline.svg?inline';
+export {default as clipboardPencilOutline} from './clipboard-pencil-outline.svg?inline';
 export {default as cogOutline} from './cog-outline.svg?inline';
 export {default as dashboardOutline} from './dashboard-outline.svg?inline';
 export {default as dotCircleOutline} from './dot-circle-outline.svg?inline';
+export {default as filtersOutline} from './filters-outline.svg?inline';
 export {default as graphsOutline} from './graphs-outline.svg?inline';
+export {default as heartPulseOutline} from './heart-pulse-outline.svg?inline';
+export {default as houseOutline} from './house-outline.svg?inline';
 export {default as infoCircleOutline} from './info-circle-outline.svg?inline';
 export {default as pencilOutline} from './pencil-outline.svg?inline';
+export {default as pillOutline} from './pill-outline.svg?inline';
 export {default as questionCircleOutline} from './question-circle-outline.svg?inline';
 export {default as searchOutline} from './search-outline.svg?inline';
 export {default as treatmentOutline} from './treatment-outline.svg?inline';
