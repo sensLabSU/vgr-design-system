dac510c6059159187ddc2a544c5e44b1182c07ae
danielrehn <daniel.rehn@consid.se>
2025-10-24 10:49:45 +0200

    Cleanup

--- index.ts
+++ index.ts
@@ -248,15 +248,15 @@ function isObject(item: any) {
     return (item && typeof item === 'object' && !Array.isArray(item));
 }
 
-function mergeDeep(target: object, ...sources: object[]) {
+function mergeDeep(target: {[key:string]: object}, ...sources: object[]) {
     if (!sources.length) return target;
-    const source = sources.shift();
+    const source: any = sources.shift();
 
     if (isObject(target) && isObject(source)) {
         for (const key in source) {
             if (isObject(source[key])) {
                 if (!target[key]) Object.assign(target, { [key]: {} });
-                mergeDeep(target[key], source[key]);
+                mergeDeep(target[key] as {[key:string]: object}, source[key]);
             } else {
                 Object.assign(target, { [key]: source[key] });
             }
@@ -266,7 +266,7 @@ function mergeDeep(target: object, ...sources: object[]) {
     return mergeDeep(target, ...sources);
 }
 
-const config = function(conf: object = {}) {
+function config(conf: object = {}) {
     return mergeDeep(defaultTailwindConfig, conf);
 }
 
