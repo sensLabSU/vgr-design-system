92cc84ee49f51e730a4cc9c7f8d0e7be8c8d36d6
danielrehn <daniel.rehn@consid.se>
2025-10-27 13:54:48 +0100

    Small fix in horizontal separator

--- core/VgrSeparator.vue
+++ core/VgrSeparator.vue
@@ -8,7 +8,7 @@ defineProps<{
   <div class="
     border-current border-t
     [[data-menu]>&]:-mx-1 [[data-menu]>&]:my-1
-    [[data-stack].horizontal>&]:border-l [[data-stack].horizontal>&]:border-t-0
+    [[data-stack].horizontal>&]:border-l [[data-stack].horizontal>&]:border-t-0 [[data-stack].horizontal>&]:h-[1.25em]
     [[data-card]>&]:my-5
     [[data-command-palette]_&]:my-3 [[data-command-palette]_&]:-mx-2
   " :class="[light ? 'text-white/25 dark:text-black/25' : 'text-black/20 dark:text-white/20']" data-separator></div>
