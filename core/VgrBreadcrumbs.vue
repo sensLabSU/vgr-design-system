806798c6d4a9482562033909d6844456b3494184
danielrehn <daniel.rehn@consid.se>
2025-10-27 12:50:09 +0100

    Minor tweak in breadcrumbs

--- core/VgrBreadcrumbs.vue
+++ core/VgrBreadcrumbs.vue
@@ -3,9 +3,10 @@
 </script>
 
 <template>
-  <ul class="flex items-center gap-2 text-xs font-light
-    [&>*:not(:first-child)]:before:content-['/'] [&>*:not(:first-child)]:before:pr-2 [&>*:not(:first-child)]:before:opacity-50
-    [&>a]:text-healthcare dark:[&>a]:text-healthcare-70 hover:[&>a]:underline
+  <ul class="flex items-center gap-2 text-xs font-light text-black dark:text-white
+    [&>*:not(:first-child)]:before:content-['/'] [&>*:not(:first-child)]:before:text-black dark:[&>*:not(:first-child)]:before:text-white
+    [&>*:not(:first-child)]:before:pr-2 [&>*:not(:first-child)]:before:opacity-50
+    [&>a]:text-healthcare dark:[&>a]:text-healthcare-70 hover:[&>a]:text-healthcare-70 dark:hover:[&>a]:text-healthcare-60
   ">
     <slot/>
   </ul>
